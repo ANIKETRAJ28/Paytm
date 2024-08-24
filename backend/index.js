632cd5aa8cfcc5358aa727bf4a70ed80cc685ab5
Aniket <aniketraj28042003@gmail.com>
2024-08-24 16:59:33 +0530

    added boiler template for backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { connectDB } = require("./src/config/db");
 const { PORT } = require("./src/config/env");
@@ -9,6 +10,13 @@ const app = express();
 const setupServer = async() => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(cors());
+    app.use((req, res) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+        res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-access-token");
+    });
 
     app.listen(PORT, async() => {
         try {
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
-    MONGO_DB_URL: process.env.MONGO_DB_URL
+    MONGO_DB_URL: process.env.MONGO_DB_URL,
+    JWT_SECRET: process.env.JWT_SECRET
 };
